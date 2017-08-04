ffaf2f9b19f51abf13abf7c643b56e205b0f0170
FlyBirdHeight <531020509@qq.com>
2017-08-05 00:13:11 +0800

    ljq

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -23,33 +23,34 @@ class WechatController extends Controller
         $wechat = app('wechat');
         $userApi = $wechat->user;
         $wechat->server->setMessageHandler(function($message) use ($userApi){
-            switch ($message->MsgType) {
-                case 'event':
-                    return '收到事件消息';
-                    break;
-                case 'text':
-                    return '你好'.$message->FromUserName;
-                    break;
-                case 'image':
-                    return '收到图片消息';
-                    break;
-                case 'voice':
-                    return '收到语音消息';
-                    break;
-                case 'video':
-                    return '收到视频消息';
-                    break;
-                case 'location':
-                    return '收到坐标消息';
-                    break;
-                case 'link':
-                    return '收到链接消息';
-                    break;
-                // ... 其它消息
-                default:
-                    return '收到其它消息';
-                    break;
-            }
+            return "欢迎关注！";
+//            switch ($message->MsgType) {
+//                case 'event':
+//                    return '收到事件消息';
+//                    break;
+//                case 'text':
+//                    return '你好'.$message->FromUserName;
+//                    break;
+//                case 'image':
+//                    return '收到图片消息';
+//                    break;
+//                case 'voice':
+//                    return '收到语音消息';
+//                    break;
+//                case 'video':
+//                    return '收到视频消息';
+//                    break;
+//                case 'location':
+//                    return '收到坐标消息';
+//                    break;
+//                case 'link':
+//                    return '收到链接消息';
+//                    break;
+//                // ... 其它消息
+//                default:
+//                    return '收到其它消息';
+//                    break;
+//            }
         });
 
         Log::info('return response.');
