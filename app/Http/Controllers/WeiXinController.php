ab94b725face552580bf111021148845f2f49087
FlyBirdHeight <531020509@qq.com>
2017-08-05 11:36:57 +0800

    ljq

--- app/Http/Controllers/WeiXinController.php
+++ app/Http/Controllers/WeiXinController.php
@@ -3,21 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 class WeiXinController extends Controller
 {
     public function api(){
-        $echoStr = $_GET["echostr"];
+        $echoStr = Input::get('echostr');
         if($this->checkSignature()){
             return $echoStr;
         }
-        return $echoStr;
+        return null;
     }
 
     private function checkSignature(){
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
+        $signature = Input::get('signature');
+        $timestamp = Input::get('timestamp');
+        $nonce     = Input::get('nonce');
         $token = "adsionlilovefxq";
         $tmpArr = array($token, $timestamp, $nonce);
         sort($tmpArr, SORT_STRING);
